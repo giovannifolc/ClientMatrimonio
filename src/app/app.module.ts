5514e124ea6fd600c06cde416236c1578ce543e8
iFederx <federico.brignone@gmail.com>
2020-12-16 23:16:26 +0100

    building home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { ResponsiveToolbarComponent } from './responsive-toolbar/responsive-toolbar.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {RouterModule} from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
 import { ListaNozzeComponent } from './lista-nozze/lista-nozze.component';
 import { GrazieComponent } from './grazie/grazie.component';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 
 @NgModule({
   declarations: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,11 @@ export class HomeComponent implements OnInit {
     let now = new Date();
     let date = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDay(), now.getHours(), now.getUTCMinutes(), now.getUTCMinutes(), now.getUTCSeconds());
     let futuredate = Date.UTC(2021, 4, 10, 10, 30, 0);
-    let missingTime= futuredate-date;
-    let days = Math.floor(missingTime/(24*3600));
-    let hours = Math.floor((missingTime - (days*24*3600))/3600);
-    let minutes = Math.floor((missingTime - (days*24*3600) - hours*3600)/60);
-    let seconds = Math.floor(missingTime - (days*24*3600) - (hours*3600) - minutes*60);
+    let missingTime = futuredate - date;
+    let days = Math.floor(missingTime / (24 * 3600));
+    let hours = Math.floor((missingTime - (days * 24 * 3600)) / 3600);
+    let minutes = Math.floor((missingTime - (days * 24 * 3600) - hours * 3600) / 60);
+    let seconds = Math.floor(missingTime - (days * 24 * 3600) - (hours * 3600) - minutes * 60);
     this.timer = days + ' Giorni ' + hours + ' Ore ' + minutes + ' Minuti ' + seconds + ' Secondi';
   });
 
