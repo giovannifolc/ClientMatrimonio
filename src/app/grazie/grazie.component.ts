d26c9f18c2e03fbdef6cc9e172bd4f4daa5890e0
iFederx <federico.brignone@gmail.com>
2021-01-22 17:57:13 +0100

    imported photos

--- src/app/grazie/grazie.component.ts
+++ src/app/grazie/grazie.component.ts
@@ -8,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class GrazieComponent implements OnInit {
 
   startIndex = 0
-  ImageData1 = ["assets/images/background_home.jpg", "assets/images/montagne.png"]
+  ImageData1 = [
+    "assets/images/grazie/od_laurea_j.jpg",
+    "assets/images/grazie/od_leti_e_compagni_liceo.jpg",
+    "assets/images/grazie/od_leti_e_mitica_segre.jpg",
+    "assets/images/grazie/od_leti_e_svizzere.jpg",
+    "assets/images/grazie/od_leti_simo_e_franco.jpg"
+  ]
 
-  ImageData2 = ["assets/images/background_home.jpg", "assets/images/montagne.png"]
+  ImageData2 = [
+    "assets/images/grazie/os_poli.jpg",
+    "assets/images/grazie/os_simo_and_co_a_serre.jpg",
+    "assets/images/grazie/os_simo_and_co_al_lago.jpg",
+    "assets/images/grazie/os_simo_e_famiglia.jpg",
+    "assets/images/grazie/os_simo_leti_e_j.jpg"
+  ]
 
-  ImageData3 = ["assets/images/background_home.jpg", "assets/images/montagne.png"]
+  ImageData3 = [
+    "assets/images/grazie/vd_leti_cossu_e_ele.jpg",
+    "assets/images/grazie/vd_leti_e_compa_uni.jpg",
+    "assets/images/grazie/vd_leti_e_famiglia.jpg",
+    "assets/images/grazie/vd_leti_ele_rovers.jpg",
+    "assets/images/grazie/vd_leti_giuli.jpg"
+  ]
 
-  ImageData4 = ["assets/images/background_home.jpg", "assets/images/montagne.png"]
-
+  ImageData4 = [
+    "assets/images/grazie/vs_simo_edo.jpg",
+    "assets/images/grazie/vs_simo_j_varo.jpg",
+    "assets/images/grazie/vs_simo_maio_nicas.jpg",
+    "assets/images/grazie/vs_simo_michi.jpg",
+    "assets/images/grazie/vs_sursum.jpg"
+  ]
 
   constructor() { }
 
