7ff5da04019e977b84973a14fe1aa5c91a3bd525
iFederx <federico.brignone@gmail.com>
2021-01-22 20:26:33 +0100

    building grazie

--- src/app/grazie/grazie.component.ts
+++ src/app/grazie/grazie.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbCarouselConfig, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-grazie',
   templateUrl: './grazie.component.html',
-  styleUrls: ['./grazie.component.css']
+  styleUrls: ['./grazie.component.scss']
 })
 export class GrazieComponent implements OnInit {
+  @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
 
-  startIndex = 0
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+  // startIndex = 0;
   ImageData1 = [
     "assets/images/grazie/od_laurea_j.jpg",
     "assets/images/grazie/od_leti_e_compagni_liceo.jpg",
@@ -40,38 +49,61 @@ export class GrazieComponent implements OnInit {
     "assets/images/grazie/vs_sursum.jpg"
   ]
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
+  // togglePaused() {
+  //   if (this.paused) {
+  //     this.carousel.cycle();
+  //   } else {
+  //     this.carousel.pause();
+  //   }
+  //   this.paused = !this.paused;
+  // }
+
+  // onSlide(slideEvent: NgbSlideEvent) {
+  //   if (this.unpauseOnArrow && slideEvent.paused &&
+  //     (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+  //     this.togglePaused();
+  //   }
+  //   if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+  //     this.togglePaused();
+  //   }
+  // }
 
   ngOnInit(): void {
-    this.Repeat();
+    // this.Repeat();
   }
 
-  Repeat() {
-    setTimeout(() => {
-      this.__FunctionSlide();
-      this.Repeat();
-    }, 2000);
-  }
+  // Repeat() {
+  //   setTimeout(() => {
+  //     this.__FunctionSlide();
+  //     // this.Repeat();
+  //   }, 8000);
+  // }
 
-  __FunctionSlide() {
-    const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
-    if (slides === []) {
-      this.Repeat();
-    }
-    for (const x of slides) {
-      const y = x as HTMLElement;
-      y.style.display = 'none';
-    }
-    if (this.startIndex > slides.length - 1) {
-      this.startIndex = 0;
-      const slide = slides[this.startIndex] as HTMLElement;
-      slide.style.display = 'block';
-      this.startIndex++;
-    } else {
+  // __FunctionSlide() {
+  //   const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
+  //   if (slides === []) {
+  //     this.Repeat();
+  //   }
+  //   for (const x of slides) {
+  //     const y = x as HTMLElement;
+  //     y.style.display = 'none';
+  //   }
+  //   if (this.startIndex > slides.length - 1) {
+  //     this.startIndex = 0;
+  //     const slide = slides[this.startIndex] as HTMLElement;
+  //     slide.style.display = 'block';
+  //     this.startIndex++;
+  //   } else {
 
-      const slide = slides[this.startIndex] as HTMLElement;
-      slide.style.display = 'block';
-      this.startIndex++;
-    }
-  }
+  //     const slide = slides[this.startIndex] as HTMLElement;
+  //     slide.style.display = 'block';
+  //     this.startIndex++;
+  //   }
+  // }
 }
