5dee692499aea8238caf687fe753afcbd61faf0e
iFederx <federico.brignone@gmail.com>
2021-02-06 18:16:40 +0100

    menu overlay

--- src/app/responsive-toolbar/responsive-toolbar.component.ts
+++ src/app/responsive-toolbar/responsive-toolbar.component.ts
@@ -1,25 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-responsive-toolbar',
   templateUrl: './responsive-toolbar.component.html',
   styleUrls: ['./responsive-toolbar.component.scss']
 })
-export class ResponsiveToolbarComponent implements OnInit {
+export class ResponsiveToolbarComponent implements OnInit, AfterViewInit {
 
   menuItems: string[] = ['Home', 'RSVP & Info', 'Lista nozze', 'Grazie'];
   navLinks = [
-    {path: '/', label: 'Home'},
-    {path: '/info', label: 'RSVP & Info'},
-    {path: '/listanozze', label: 'Lista nozze'},
-    {path: '/grazie'}
+    { path: '/', label: 'Home' },
+    { path: '/info', label: 'RSVP & Info' },
+    { path: '/listanozze', label: 'Lista Nozze' },
+    { path: '/grazie', label: 'Grazie' }
 
   ];
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) {
 
-  ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    $('#openNav').on('click', function () {
+      $('#myNav').css("width", "100vw");
+    });
+
+    $('#closeNav').on('click', function () {
+      $('#myNav').css("width", "0%");
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
